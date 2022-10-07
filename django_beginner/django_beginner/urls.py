985a75c909d714cb32b0198af859d693b9652776
Jino Basilio <sonnysinzanity@gmail.com>
2022-10-07 14:00:48 +0800

    Added new url for members

--- django_beginner/django_beginner/urls.py
+++ django_beginner/django_beginner/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from members import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('members/', views.index, name='index')
 ]
